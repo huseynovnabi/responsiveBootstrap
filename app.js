841f48f6eac1f488f8a8ec62a8ac51f1eada24ae
huseynovnabi <131556891+huseynovnabi@users.noreply.github.com>
2023-12-22 21:08:31 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(document).ready(function () {
+    // ekran yuklenende 992px genislikden kicik ve ya beraber olanda isleyir bu funksiyalar
+    if ($(window).width() <= 992) {
+        $("#hamburgerMenu").click(function () {
+            $("nav a").slideToggle(); // linklerin gizledilib ve ya gorsedilmesi
+            $(this).toggleClass("fa-bars fa-times"); // icon change
+        });
+
+        $("nav a").click(function () {
+            $("nav a").slideUp(); // linklerin yuxariya dogru gizledilmesi
+            $("#hamburgerMenu").removeClass("fa-times").addClass("fa-bars"); // cancel iconun legv edib menu iconun elave edir 
+        });
+    }
+});
+
+
+
+
+
