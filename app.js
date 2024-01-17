27710a0c50f1b361dea135c0589974f7696cd180
huseynovnabi huseynovnabi <huseynovnabi2002@gmail.com>
2024-01-17 13:02:56 +0000

    styleChangeHeader

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-"use strict";
+// "use strict";
 
-$(document).ready(function () {
-    // ekran yuklenende 992px genislikden kicik ve ya beraber olanda isleyir bu funksiyalar
-    if ($(window).width() <= 992) {
-        $("#hamburgerMenu").click(function () {
-            $("nav a").slideToggle(); // linklerin gizledilib ve ya gorsedilmesi
-            $(this).toggleClass("fa-bars fa-times"); // icon change
-        });
+// $(document).ready(function () {
+//     // ekran yuklenende 992px genislikden kicik ve ya beraber olanda isleyir bu funksiyalar
+//     if ($(window).width() <= 992) {
+//         $("#hamburgerMenu").click(function () {
+//             $("nav a").slideToggle(); // linklerin gizledilib ve ya gorsedilmesi
+//             $(this).toggleClass("fa-bars fa-times"); // icon change
+//         });
 
-        $("nav a").click(function () {
-            $("nav a").slideUp(); // linklerin yuxariya dogru gizledilmesi
-            $("#hamburgerMenu").removeClass("fa-times").addClass("fa-bars"); // cancel iconun legv edib menu iconun elave edir 
-        });
-    }
-});
+//         $("nav a").click(function () {
+//             $("nav a").slideUp(); // linklerin yuxariya dogru gizledilmesi
+//             $("#hamburgerMenu").removeClass("fa-times").addClass("fa-bars"); // cancel iconun legv edib menu iconun elave edir 
+//         });
+//     }
+// });
 
 
 
